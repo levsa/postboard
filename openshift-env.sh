43f7ddf14fbf6c66850495b040ea052086df5a67
Levon Saldamli <levon.sa@gmail.com>
2015-07-26 02:55:09 +0200

    Added openshift env variables. Removed test text in login page.

---
+++ openshift-env.sh
@@ -0,0 +1,2 @@
+rhc env-set GOOGLE_ID='620808743029-eo0ffngtrsv7brv6hn1bfhp2ninkgcpo.apps.googleusercontent.com' --app postboard
+rhc env-set GOOGLE_SECRET='iBSmBDJED9tKNVFRiUcXYY4V' --app postboard
